da4aa18f1d91d3ee2ab32b287b81ff0f2df74b5d
AxVol <68782579+AxVol@users.noreply.github.com>
2024-07-21 17:45:58 +0300

    tests in notification sender

---
+++ Microservices/NotificationSender/NotificationSender.Tests/Mock/MockFactory.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using Moq;
+using NotificationSender.DAL.Repository.Interfaces;
+using NotificationSender.Domain.Emun;
+using NotificationSender.Domain.Entity;
+
+namespace NotificationSender.Tests.Mock;
+
+public static class MockFactory<T> where T : class
+{
+    public static IRepository<Notification> GetNotificationRepository()
+    {
+        Notification notification = GetNotification();
+        var mock = new Mock<IRepository<Notification>>();
+
+        mock.Setup(m => m.AddAsync(It.IsAny<Notification>())).Returns(Task.CompletedTask);
+        mock.Setup(m => m.DeleteAsync(It.IsAny<Notification>())).Returns(Task.CompletedTask);
+        mock.Setup(m => m.UpdateAsync(It.IsAny<Notification>())).Returns(Task.CompletedTask);
+        mock.Setup(m => m.GetAsync(It.IsAny<Notification>())).Returns(Task.FromResult(notification));
+
+        return mock.Object;
+    }
+    
+    public static ILogger<T> GetLogger()
+    {
+        var mock = new Mock<ILogger<T>>();
+
+        return mock.Object;
+    }
+
+    private static Notification GetNotification()
+    {
+        string uuid = "4dfc6b14-7213-3363-8009-b23c56e3a1b1";
+        Guid guid;
+        bool isGuid = Guid.TryParse(uuid, out guid);
+        string uuidStatus = "4dfc6b14-7213-2002-8009-b23c56e3a1b1";
+        Guid notificationStatusGuid;
+        bool isGuidStatus = Guid.TryParse(uuidStatus, out notificationStatusGuid);
+
+        Notification notification = new Notification()
+        {
+            Id = guid,
+            Title = "test",
+            Body = "test",
+            TimeToSend = DateTime.Now.ToUniversalTime(),
+            IsSend = false,
+            ForUsers = new List<NotificationStatus>()
+            {
+                new NotificationStatus()
+                {
+                    Id = notificationStatusGuid,
+                    Email = "test@mail.ru",
+                    SendStatus = false
+                }
+            },
+            CreatorName = "test",
+            NotificationState = NotificationState.NotStarted,
+        };
+        notification.SetNotificaitonCount(3);
+        
+        return notification;
+    }
+}
---
+++ Microservices/NotificationSender/NotificationSender.Tests/NotificationServiceTests.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Extensions.Logging;
+using NotificationSender.Application.Interfaces;
+using NotificationSender.Application.Services;
+using NotificationSender.DAL.Repository.Interfaces;
+using NotificationSender.Domain.Dto;
+using NotificationSender.Domain.Emun;
+using NotificationSender.Domain.Entity;
+using NotificationSender.Domain.Response;
+using Xunit;
+
+namespace NotificationSender.Tests;
+
+public class NotificationServiceTests
+{
+    private readonly INotificationService notificationService;
+
+    public NotificationServiceTests()
+    {
+        IRepository<Notification> repository = Mock.MockFactory<NotificationService>.GetNotificationRepository();
+        ILogger<NotificationService> logger = Mock.MockFactory<NotificationService>.GetLogger();
+        notificationService = new NotificationService(repository, logger);
+    }
+
+    [Fact]
+    public async Task Get_Exists_Notification_State_Test()
+    {
+        // Arrage
+        Guid guid;
+        bool isGuid = Guid.TryParse("4dfc6b14-7213-3363-8009-b23c56e3a1b1", out guid);
+        NotificationDto dto = new NotificationDto();
+        dto.Id = guid;
+        dto.PublishDate = DateTime.Now;
+        
+        // Act
+        NotificationStateResponse response = await notificationService.GetNotificationStateAsync(dto);
+
+        // Assert
+        Assert.Equal(ResponseStatus.Success, response.Status);
+        Assert.Equal("NotStarted", response.Value);
+    }
+
+    [Fact]
+    public async Task Get_Processed_Users_Test()
+    {
+        // Arrage
+        Guid guid;
+        bool isGuid = Guid.TryParse("4dfc6b14-7213-3363-8009-b23c56e3a1b1", out guid);
+        NotificationDto dto = new NotificationDto();
+        dto.Id = guid;
+        dto.PublishDate = DateTime.Now;
+        
+        // Act
+        UsersProcessedResponse response = await notificationService.GetProcessedUsersAsync(dto);
+
+        // Assert
+        Assert.Equal(2, response.Value);
+        Assert.Equal(ResponseStatus.Success, response.Status);
+    }
+}
