875f32e7780c59e4a6453f0b3d854e276bd4ec68
AxVol <68782579+AxVol@users.noreply.github.com>
2024-08-24 18:39:59 +0300

    information in swager

--- Microservices/NotificationSender/NotificationSender.API/Controllers/NotificationController.cs
+++ Microservices/NotificationSender/NotificationSender.API/Controllers/NotificationController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
 using NotificationSender.Application.Interfaces;
 using NotificationSender.Domain.Dto;
 using NotificationSender.Domain.Emun;
@@ -17,9 +18,27 @@ public class NotificationController
         notificationService = service;
     }
     
+    /// <remarks>
+    /// Данный эндпоинт принимает id нотификации в виде uuid и возвращает данные о запрошеной нотификации
+    ///
+    ///     GET /Notification
+    ///     {
+    ///         "id": uuid
+    ///     }
+    ///
+    ///      string - Статус нотификации
+    ///      (
+    ///          InProgress - Нотификация в обработке 
+    ///          NotStarted - Время отправки ещё не наступило,
+    ///          Finished - Нотификация обработана
+    ///      )
+    /// </remarks>
+    /// <response code="200">Данные успешно отправлены</response>
+    /// <response code="400">Для большей инофрмации об ошибке, смотрите описание ошибки в json файле</response>
     [HttpGet]
-    public async Task<IActionResult> GetNotificationState(NotificationDto notification)
+    public async Task<IActionResult> GetNotificationState(Guid id, DateTime publishDate)
     {
+        NotificationDto notification = new NotificationDto(){ Id = id, PublishDate = publishDate };
         NotificationStateResponse notificationState = await notificationService.GetNotificationStateAsync(notification);
 
         if (notificationState.Status == ResponseStatus.Success)
--- Microservices/NotificationSender/NotificationSender.API/Controllers/ProcessedController.cs
+++ Microservices/NotificationSender/NotificationSender.API/Controllers/ProcessedController.cs
@@ -17,9 +17,22 @@ public class ProcessedController
         notificationService = service;
     }
     
+    /// <remarks>
+    /// Данный эндпоинт принимает id нотификации в виде uuid и возвращает данные о запрошеной нотификации
+    ///
+    ///     GET /Notification
+    ///     {
+    ///         "id": uuid
+    ///     }
+    ///
+    ///      int - Количество пользваталей которые получили нотификацию
+    /// </remarks>
+    /// <response code="200">Данные успешно отправлены</response>
+    /// <response code="400">Для большей инофрмации об ошибке, смотрите описание ошибки в json файле</response>
     [HttpGet]
-    public async Task<IActionResult> GetProcessedUsers(NotificationDto notification)
+    public async Task<IActionResult> GetProcessedUsers(Guid id, DateTime publishDate)
     {
+        NotificationDto notification = new NotificationDto(){ Id = id, PublishDate = publishDate };
         UsersProcessedResponse notificationResponse = await notificationService.GetProcessedUsersAsync(notification);
     
         if (notificationResponse.Status == ResponseStatus.Success)
